f9dd149f59a8ead633c9b45fd47db51bdcc4cb15
nouse4anick <git@nouse4anick.com>
2013-05-13 21:47:45 -0700

    Slight code cleanup
    Updated license info, did small cleanup (and spell check), commented out some code blocks to be removed later after a debug and test session

--- gpsdata.py
+++ gpsdata.py
@@ -2,7 +2,9 @@
 # Modified code: Art Miller - proto-type for Steam boat logging program.
 #   Also includes shutdown procedure, also has (to the best of my ability) comments for 
 #   all vars that are reported (at least that are in here)
-# Orginal code: Written by Dan Mandle http://dan.mandle.me September 2012 License: GPL 2.0
+# LICENSE: Until I've read the GPLv3 license, I'm just going to say that you are free to modify and/or
+#   use in part or in whole any of the code below as long as Dan and I are credited as 'starting points'
+# Original code: Written by Dan Mandle http://dan.mandle.me September 2012 License: GPL 2.0
 
 # TODO: clean code, proof, debug, test
 
@@ -14,9 +16,10 @@ import time
 import threading
 import RPi.GPIO as GPIO
 
-gpsd = None #seting the global variable
+gpsd = None #setting the global variable
 
 class HaltPress( Exception ): pass
+
 # gps polling thread:
 class GpsPoller(threading.Thread):
     def __init__(self):
@@ -30,6 +33,7 @@ class GpsPoller(threading.Thread):
         global gpsd
         while gpsp.running:
             gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+
 # 'debug' gps data output
 def gpsDebugoutput():
     global gpsd
@@ -39,14 +43,15 @@ def gpsDebugoutput():
     print '----------------------------------------'
     print 'latitude      ' , gpsd.fix.latitude
     print 'longitude     ' , gpsd.fix.longitude
-    #print 'time utc      ' , gpsd.utc,' + ', gpsd.fix.time # not really needed, onboard RTC, only use when no RTC is present
+    #print 'time utc      ' , gpsd.utc,' + ', gpsd.fix.time # not really needed, on-board RTC, only use when no RTC is present
     print 'altitude (ft) ' , gpsd.fix.altitude * METERS_TO_FEET
     #print 'eps         ' , gpsd.fix.eps * MPS_TO_MPH #speed error
     #print 'epd         ' , gpsd.fix.epd #Heading error
-    #print 'epx         ' , gpsd.fix.epx * METERS_TO_FEET #horizontal x error
-    #print 'epy         ' , gpsd.fix.epy * METERS_TO_FEET #horizontal y error
+    #print 'epx         ' , gpsd.fix.epx * METERS_TO_FEET #horizontal x error NOTE: Same as y error
+    #print 'epy         ' , gpsd.fix.epy * METERS_TO_FEET #horizontal y error NOTE: same as x error
     #print 'epv         ' , gpsd.fix.epv * METERS_TO_FEET #vertical error
-    #print 'ept         ' , gpsd.fix.ept #?
+    #print 'ept         ' , gpsd.fix.ept # time error
+    #print 'epd         ' , gpsd.fix.epd # ?
     print 'speed (MPH)   ' , gpsd.fix.speed * MPS_TO_MPH # speed MIGHT be in knots. need testing
     #print 'climb         ' , gpsd.fix.climb * MPS_TO_MPH
     print 'heading       ' , gpsd.fix.track # heading
@@ -65,6 +70,7 @@ def blink(pin):
 
 # maybe add this in later?: GPIO.add_event_callback(channel, my_callback, bouncetime=200)
 def buttonPress():
+    print "shutdown button pressed"
     raise HaltPress
 
 if __name__ == '__main__':
@@ -72,8 +78,7 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(17, GPIO.IN) # switch for turning the pi off
     GPIO.setup(18, GPIO.OUT) # led to notify when the pi is recording
-    # event here
-    GPIO.add_event_callback(17, buttonPress(), bouncetime=200)
+    GPIO.add_event_callback(17, buttonPress(), bouncetime=200) # raise a halt program exception
     
     gpsp = GpsPoller() # create the thread
     shutdownbutton = False # used for figuring out if the program needs to call the shutdown routine
@@ -81,7 +86,7 @@ if __name__ == '__main__':
         GPIO.output(18,GPIO.HIGH)
         curtime = time.strftime("%m-%d-%y %H:%M")
         f = open('/home/pi/logger/logs/' + curtime + '.log','w')
-        f.write("time,lat,lon,alt,spd,head\n")
+        f.write("time,lat,lon,alt,head,spd\n")
         gpsp.start() # start it up
         while True:
             #It may take a second or two to get good data
@@ -90,21 +95,22 @@ if __name__ == '__main__':
             lat = gpsd.fix.latitude
             lon = gpsd.fix.longitude
             alt = gpsd.fix.altitude * METERS_TO_FEET
-            spd = gpsd.fix.speed * MPS_TO_MPH
+            spd = gpsd.fix.speed #* MPS_TO_MPH #not sure if base is in mps or knots... maybe record both base units and km/h?
             head = gpsd.fix.track
-            strlog = "{0},{1},{2},{3:.2},{4:.1},{5:.1}\n".format(time.time(), lat, lon,  alt, spd, head)
+            strlog = "{0},{1},{2},{3:.2},{4:.1},{5:.1}\n".format(time.time(), lat, lon,  alt, head, spd)
             if gpsd.fix.mode == 3 or gpsd.fix.mode == 2:
                 f.write(strlog)
                 f.flush()
             elif gpsd.fix.mode == 1:
                 # sleep for a tiny bit and see if the gps gets a fix
                 time.sleep(5) # 5 sec seems good enough, every 5 sec + main loop wait gives us some time to get a fix
-            if GPIO.input(17) == GPIO.HIGH:
-                print "shutdown button pressed"
-                gpsp.running = False
-                gpsp.join()
-                shutdownbutton = True
-                break
+            # not needed, event handling 
+            #if GPIO.input(17) == GPIO.HIGH:
+            #    print "shutdown button pressed"
+            #    gpsp.running = False
+            #    gpsp.join()
+            #    shutdownbutton = True
+            #    break
             time.sleep(1) # in seconds, eventually plan on a 1+ min wait, for now get as much data as possible in a short time for testing
     
     except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
@@ -120,12 +126,12 @@ if __name__ == '__main__':
             if GPIO.input(17) == GPIO.HIGH:
                 #time to exit
                 shutdownbutton = True
-                f.write("Error occured\n")
+                f.write("Error occurred\n")
                 break
     finally: # close the file and clean up
         gpsp.running = False
         gpsp.join()
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(18, GPIO.LOW) #just in-case it was left on for what ever reason
         f.flush()
         f.close()
         GPIO.cleanup()
